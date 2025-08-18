d712dc1710b37fb495acc03ed70bfad0c8afbc78
giovannacolaneri <giovannacolanericbg@gmail.com>
2025-08-18 20:39:49 -0300

    atualizacao

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,14 @@
 
-self.addEventListener('install', event =>{
+self.addEventListener('install', (event )=>{
     console.log('Service Worker: Instalado');
+    caches.open ("pwa").then((cache)=>{
+        console.log("cache aberto")
+        cache.add(index.html)
+    });
 });
-self.addEventListener('activate', event =>{
+self.addEventListener('activate', (event) =>{
     console.log('Service Worker: Ativado');
 });
-self.addEventListener('fetch', event =>{
+self.addEventListener('fetch', (event) =>{
     event.respondWith(fetch(event.request));
 });
