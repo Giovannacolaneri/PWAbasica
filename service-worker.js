2761d1e03e37fe16ff88da59e15029b672e35e4b
Giovannacolaneri <giovannacolanericbg@gmail.com>
2025-08-25 20:34:51 -0300

    deploy

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,30 @@
+const Cache_name = "tarefas";
+const Cache_assets =
+    ["/PWAbasica/index.html",
+        "/PWAbasica/manifest.json",
+        "/PWAbasica/script.js",
+        "/PWAbasica/service-worker.js",
+        "/PWAbasica/icon-192x192.png", "/PWAbasica/icon-512x512.png"
 
-self.addEventListener('install', (event )=>{
-    console.log('Service Worker: Instalado');
-    caches.open ("pwa").then((cache)=>{
+    ];
+
+
+self.addEventListener('install', (event) => {
+    /* console.log('Service Worker: Instalado');*/
+    caches.open("pwa").then((cache) => {
         console.log("cache aberto")
-        cache.addAll(["/PWAbasica/index.html",
-          "/PWAbasica/manifest.json", "/PWAbasica/script.js", "/PWAbasica/service-worker.js"
-        ]);
+        cache.addAll([Cache_assets]);
     });
 });
-self.addEventListener('activate', (event) =>{
+
+self.addEventListener('activate', (event) => {
     console.log('Service Worker: Ativado');
-    
+
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(fetch(event.request))
     caches.match(event.request).then((response) => {
         return response || fetch(event.request);
-        })
-   });
+    })
+});
